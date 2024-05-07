74d6c6751ba12a3704b75fade066117eac764b53
Jyun-Wei, Chen <84979672+ruyadisk@users.noreply.github.com>
2024-05-08 04:45:55 +0800

    Time: 118 ms (64.51%), Space: 17.3 MB (27.14%) - LeetHub

--- 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.py
+++ 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.py
@@ -1,21 +1,20 @@
 class Solution:
     def __init__(self):
-        self.vowel = ['a','e','i','o','u']
-        self.res = 0
-        self.cnt = 0
+        vowel = ['a','e','i','o','u']
+        res = 0
+        cnt = 0
     def maxVowels(self, s: str, k: int) -> int:
-        # if k == 1:
-        #     for c in s:
-        #         if c in self.vowel:
-        #             return 1
+        vowel = ['a','e','i','o','u']
+        res = 0
+        cnt = 0
         for _ in range(0, k):
-            if s[_] in self.vowel:
-                self.res += 1
-        self.cnt = self.res
+            if s[_] in vowel:
+                res += 1
+        cnt = res
         for _ in range(k, len(s)):
-            if s[_-k] not in self.vowel and s[_] in self.vowel:
-                self.cnt += 1
-                self.res = self.cnt if self.cnt > self.res else self.res
-            elif s[_-k] in self.vowel and s[_] not in self.vowel:
-                self.cnt  = self.cnt - 1 if self.cnt > 0 else 0
-        return self.res
+            if s[_-k] not in vowel and s[_] in vowel:
+                cnt += 1
+                res = cnt if cnt > res else res
+            elif s[_-k] in vowel and s[_] not in vowel:
+                cnt = cnt - 1 if cnt > 0 else 0
+        return res
