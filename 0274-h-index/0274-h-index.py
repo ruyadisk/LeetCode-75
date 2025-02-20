2a91cfe2c21e1e326ae813396aea58a84c699514
Jyun-Wei, Chen <84979672+ruyadisk@users.noreply.github.com>
2025-02-20 14:12:13 +0800

    Time: 3 ms (27.29%), Space: 18 MB (62.67%) - LeetHub

---
+++ 0274-h-index/0274-h-index.py
@@ -0,0 +1,13 @@
+class Solution:
+    def hIndex(self, citations: List[int]) -> int:
+        tbl = [0] * 1001
+        for i in citations:
+            tbl[i] += 1 
+        for j in range(len(tbl)-1, 0, -1):
+            if tbl[j] >= j:
+                return j
+            else:
+                tbl[j-1] += tbl[j]
+        print(tbl)
+        return 0
+                
